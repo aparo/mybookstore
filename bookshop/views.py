dc3ccfe430e67d11d0933c855ad57c132e408b2a
Alberto Paro <alberto.paro@gmail.com>
2013-07-28 23:47:03 +0200

    Added views to bookshop app

--- bookshop/views.py
+++ bookshop/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.shortcuts import render
+from django.http import Http404
+from bookshop.models import Book
+
+def index(request):
+    context = {'books': Book.objects.all()}
+    return render(request, 'shop/index.html', context)
+
+
+def detail(request, book_id):
+    try:
+        book = Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        raise Http404
+    return render(request, 'shop/detail.html', {'book': book})
+
